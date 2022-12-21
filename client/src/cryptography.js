7c909cfd40faadb8432acefd1c37634ccd901036
alon651 <52508633+alon651@users.noreply.github.com>
2022-12-21 11:52:01 +0200

    :)

--- client/src/cryptography.js
+++ client/src/cryptography.js
@@ -14,14 +14,14 @@ export function encrypt(input) {
     cipher.update(forge.util.createBuffer(input));
     cipher.finish();
     var encrypted = cipher.output;
-    return encrypted.toHex();
+    console.log(encrypted);
+    console.log(decrypt(encrypted));
+    return encrypted;
 }
 function decrypt(encrypted) {
     var decipher = forge.cipher.createDecipher("AES-CBC", key);
     decipher.start({ iv: iv });
     decipher.update(encrypted);
     var result = decipher.finish(); // check 'result' for true/false
-
-    // outputs decrypted hex
-    console.log(decipher.output.toString());
+    return JSON.parse(decipher.output.toString());
 }
